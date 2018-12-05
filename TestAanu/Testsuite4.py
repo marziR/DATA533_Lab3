59781366d23a1486c89abbb7fd96daf445fa228c
Aanu-A <aanuabayom@gmail.com>
2018-12-05 09:40:43 -0800

    Update

--- TestAanu/Testsuite4.py
+++ TestAanu/Testsuite4.py
@@ -4,15 +4,9 @@
 # In[2]:
 
 
-
-
-
-# In[4]:
-
-
 import unittest
-from TestAanu.TestModules.TestageModule import calc
-from TestAanu.TestModules.TestsalaryModule import bmicat
+from TestAanu.TestModules.TestmeasureModule import TestCalc
+from TestAanu.TestModules.TestclassifyModule import TestBmicat
 from TestAanu.TestModules.TestageModule import TestAgeCalc
 from TestAanu.TestModules.TestsalaryModule import TestNetSalary
 
@@ -20,10 +14,11 @@ from TestAanu.TestModules.TestsalaryModule import TestNetSalary
 def my_suite():
     suite = unittest.TestSuite()
     result = unittest.TestResult()
-    suite.addTest(unittest.makeSuite(calc))
-    suite.addTest(unittest.makeSuite(bmicat))
+    suite.addTest(unittest.makeSuite(TestCalc))
+    suite.addTest(unittest.makeSuite(TestBmicat))
     suite.addTest(unittest.makeSuite(TestAgeCalc))
     suite.addTest(unittest.makeSuite(TestNetSalary))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 my_suite()
+
